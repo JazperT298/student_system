1057a1b633ad205199b616156bb71d6d6f64f403
jazper298 <metallica298>
2020-01-21 14:05:43 +0800

    Initial Commit

---
+++ database/migrations/2020_01_21_050209_create_levels_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLevelsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('levels', function (Blueprint $table) {
+            $table->bigIncrements('level_id');
+            $table->string('level');
+            $table->integer('course_id')->unsigned();
+            $table->longText('level_description');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('levels');
+    }
+}
